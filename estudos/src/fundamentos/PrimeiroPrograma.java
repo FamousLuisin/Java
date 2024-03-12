cac32d9b798cd220197cea4d3257171e347c7322
FamousLuisin <67298012+FamousLuisin@users.noreply.github.com>
2024-03-12 20:01:17 -0300

    Estudos Java dia 01

---
+++ estudos/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		//ctrl + shift + espaço = autocomplete
+		//ctrl + shift + f = indentazação e ajuste do codigo
+		System.out.println("Hello world");
+	}
+}
