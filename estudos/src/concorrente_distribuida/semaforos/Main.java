d492a2e5a67ddf7d8be6ec94a44b3a96f5eb9be0
FamousLuisin <67298012+FamousLuisin@users.noreply.github.com>
2024-05-05 15:35:06 -0300

    Estudo de semaforos

---
+++ estudos/src/concorrente_distribuida/semaforos/Main.java
@@ -0,0 +1,16 @@
+package concorrente_distribuida.semaforos;
+
+import java.util.concurrent.Semaphore;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Semaphore estacionamento = new Semaphore(10);
+		
+		for(int i = 0;i < 20;i++) {
+			new Carro("carro " + i, estacionamento).start();
+		}
+
+	}
+
+}
