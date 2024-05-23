e4d36d89100432105e4a6d600616603058b65c8d
FamousLuisin <67298012+FamousLuisin@users.noreply.github.com>
2024-05-23 17:33:00 -0300

    Criando classe Assento e Cliente

---
+++ estudos-jpa/src/main/java/modelo/umpraum/Assento.java
@@ -0,0 +1,43 @@
+package modelo.umpraum;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "assentos")
+public class Assento {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long ind;
+	
+	private String nome;
+	
+	public Assento() {
+		
+	}
+
+	public Assento(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getInd() {
+		return ind;
+	}
+
+	public void setInd(Long ind) {
+		this.ind = ind;
+	}
+	
+}
